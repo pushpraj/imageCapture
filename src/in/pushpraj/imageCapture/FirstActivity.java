1b73c9f16b09a1fce0ffd1a9572f15366d015a8f
PushpRaj Agrawal <pushpraj.agrawal@gmail.com>
2012-03-07 11:07:45 +0530

    Add project with sample code.

---
+++ src/in/pushpraj/imageCapture/FirstActivity.java
@@ -0,0 +1,15 @@
+package in.pushpraj.imageCapture;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
