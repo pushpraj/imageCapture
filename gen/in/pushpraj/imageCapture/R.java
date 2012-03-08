b6402f9093100bfc152b4786ef3c6b60ac105b27
PushpRaj Agrawal <pushpraj.agrawal@gmail.com>
2012-03-08 14:17:24 +0530

    Add form as first page.

--- gen/in/pushpraj/imageCapture/R.java
+++ gen/in/pushpraj/imageCapture/R.java
@@ -11,7 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class id {
-        public static final int my_button=0x7f040000;
+        public static final int my_button=0x7f040002;
+        public static final int password=0x7f040001;
+        public static final int username=0x7f040000;
     }
     public static final class layout {
         public static final int main=0x7f020000;
